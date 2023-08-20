e47edb9f2f1823f62084ad9a3562c3546460f781
aufftool <129205104+aufftool@users.noreply.github.com>
2023-08-20 15:13:06 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '94ff1c245deffb6d3bf2a70ef241990e',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/94ff1c245deffb6d3bf2a70ef241990e/invoke.js"></scr' + 'ipt>');
